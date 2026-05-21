ec44ef41b35043a23085b7da8b801ba07f25ac5a
Eric Stephenson <ericb.stephenson@gmail.com>
2026-05-21 15:35:11 -0700

    add project migrations and seeds, run migrations to generate schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,46 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Seeding database..."
+
+User.find_or_create_by(email: "admin@example.com") do |user|
+  user.name = "Admin User",
+  user.password = "SecurePassword123",
+  user.is_admin = true
+end
+
+puts "Created Admin User."
+
+puts "Creating 10 sample users..."
+
+10.times do
+  User.find_or_create_by(email: Faker::Internet.email) do |user|
+    user.name = Faker::Name.name
+    user.password = "password"
+    user.is_admin = false
+  end
+end
+
+puts "Sample users are in place."
+
+puts "Creating courses..."
+
+Course.find_or_create_by(title: "Rails API") do |course|
+  course.description = "Learn to build an API in Ruby on Rails."
+  course.status = "draft"
+end
+
+Course.find_or_create_by(title: "Rails with MongoDB") do |course|
+  course.description = "Learn how to build a NoSQL database in Rails with MongoDB."
+  course.status = "draft"
+end
+
+Course.find_or_create_by(title: "Rails with GraphQL") do |course|
+  course.description = "Learn how to build a GraphQL API in Rails."
+  course.status = "draft"
+end
+
+puts "Courses created."
+
+puts "Finished seeding."
