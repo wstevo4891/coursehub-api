59978cf17a52fcbb0e78f4d7d37fb9264dc3da04
Eric Stephenson <ericb.stephenson@gmail.com>
2026-05-22 16:10:54 -0700

    add UserSerializer

---
+++ app/serializers/user_serializer.rb
@@ -0,0 +1,27 @@
+class UserSerializer < ActiveModel::Serializer
+  include Rails.application.routes.url_helpers
+
+  attributes :id, :name, :email, :is_admin, :created_at, :updated_at
+
+  # ToDo: Enable the code below when users have avatar urls
+  # attributes :id, :name, :email, :is_admin, :created_at, :updated_at, :avatar_url
+
+  # def avatar_url
+  #   if object.avatar.attached?
+  #     # Option 1: url_for
+  #     # url_for(object.avatar)
+
+  #     # Option 2: rails_blob_url (explicit alias for url_for)
+  #     # rail_blob_url(object.avatar, only_path: false)
+
+  #     # Option 3: service_url
+  #     # direct link, often temporary for cloud
+  #     # Make sure 'routes.default_url_options' are set for the host.
+  #     object.avatar.service_url
+  #   else
+  #     # Return nil or a default avatar URL if no avatar is attached
+  #     # e.g., "https://gravatar.com/avatar/#{Digest::MD5.hexdigest(object.email)}?d=identicon"
+  #     nil
+  #   end
+  # end
+end
