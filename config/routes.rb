31178a2a9a91651c4625df2890d8eb5ae4eab3a1
Eric Stephenson <ericb.stephenson@gmail.com>
2026-05-22 16:50:26 -0700

    mount Sidekiq Web engine at "/sidekiq" in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+require "sidekiq/web"
+
 Rails.application.routes.draw do
   mount Rswag::Ui::Engine => "/api-docs"
   mount Rswag::Api::Engine => "/api-docs"
+
+  mount Sidekiq::Web => "/sidekiq"
 
   namespace :api do
     namespace :v1 do
