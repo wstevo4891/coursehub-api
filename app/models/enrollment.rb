525680788f8a7b4e1d4cf4099fa7053ef900993b
Eric Stephenson <ericb.stephenson@gmail.com>
2026-05-22 13:52:18 -0700

    propagate changes in enrollments to their associations with touch: true

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,4 +1,9 @@
+# frozen_string_literal: true
+
+# Using touch: true will cause the associated User and Course records
+# to update their updated_at timestamp whenever their Enrollment is saved.
+# This technique helps to invalidate fragment caches that use versioned keys.
 class Enrollment < ApplicationRecord
-  belongs_to :user
-  belongs_to :course
+  belongs_to :user, touch: true
+  belongs_to :course, touch: true
 end
